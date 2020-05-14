0db0365d5e345bea48edff7180e730108be8466c
ArgorMaximus <bnsiddhanth0@gmail.com>
2020-05-14 18:06:24 +0530

    Initial commit

---
+++ udacity-c3-restapi-feed/src/config/config.ts
@@ -0,0 +1,27 @@
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host":process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_reigion": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket":process.env.AWS_MEDIA_BUCKET,
+    "url": process.env.URL
+  },
+  "prod": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host":process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_reigion": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket":process.env.AWS_MEDIA_BUCKET,
+    "url": process.env.URL
+  },
+  "jwt":{
+    "secret": process.env.JWT_SECRET
+  }
+}
